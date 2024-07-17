b031df6569a1a919c0d3865dd98b06a308098b69
Alexandr Lukanev <lukanevteach@gmail.com>
2024-07-17 15:49:17 +0300

    added prefix. updated env

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+  
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Включите автоматическое преобразование
